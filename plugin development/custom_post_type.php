cc9e4fa554125101d3a52c9ff15953067bc8ac45
miroslavgr <43068250+miroslavgr@users.noreply.github.com>
2019-01-11 13:04:59 +0200

    Update custom_post_type.php

--- plugin development/custom_post_type.php
+++ plugin development/custom_post_type.php
@@ -1,25 +1,55 @@
 <?php
 /*
-	This file is about custom post type create and use.
-
+    This file is about custom post type create and use.
 	5 default types
 	post
 	page
 	attachment - media uploaded and attached to post type entries
 	revision - revision ofa post type used as backup and can be restored
 	nav menus - Menu items added to a nav menu
-
 	always use init on register
-	
+
+    Create single-(post_type).php for showing a single item
+    and archive-(post_type).php for a blog of items
+    Search for this functions followed by 'example'
     Overall functions 
 */
-register_post_type( 'type_name', ['public' => 'true'] );// 1st param name of type
+//core functions
 flush_rewrite_rules();//clear the permalinks after the post type has been registered
 register_activation_hook();//on plugin activation
 register_deactivation_hook();//on plugin deactivation
+//register new post type on 'init' action hook and no more than 20 chars
+register_post_type( 'type_name', ['public' => 'true'] );// 1st param name of type
+register_taxonomy('post_tag', 'post_type');// mandatory if want to attach a taxanomy
+post_type_exists('post_type');//boolean
+get_post_types($args, $output, $operator);//return all post type existing
+get_post_type($post); // or post->id, if empty current post used //return the post type
+add_post_type_support('post_type', $supports);// alter a post type and more support
+remove_post_type_support('post_type', $supports);// remove 
+set_post_type($post_id, 'post_type');//change the post type of a post entry
 
+/*post meta
+uniqe- true for only 1 possible entry for this post.
+_$meta_key - for hiding it from the editor
+*/
+add_post_meta($post_id, $meta_key, $meta_value, $uniqe);// add custom field to a post
+update_post_meta($post_id, $meta_key, $meta_value, $prev_value);//perfrom create if not exist, prev_val is optional if not uniqe
+delete_post_meta($post_id, $meta_key, $meta_value);// $meta_value is optional needed if not uniqe
+get_post_meta($post_id, $meta_key, $single);// 3rd param true if uniqe, otherwise false to return array
+get_post_custom($post_id);//return multi dimensial array of all meta data existing for a post
+/*
+    Metaboxes creation must be attachd to add_action('add_meta_box','cb') hook 
+    They are already in a <form> and already have Submit buttons from the editor
+*/
+add_meta_box('box_name_id','Box title','html_box_cb','post_type');
+remove_meta_box('box_name_id','Box title','html_box_cb','post_type'); 
+//my functions
+display_custom_posts();//example with WP_query
+exm_get_post_types();//get_post_types example
 
 //Main Example
+
+// Start Create Post Type and flush
 function pluginprefix_setup_post_type() {
    //text labels for admin menu
 	$labels = array(
@@ -47,15 +77,18 @@ function pluginprefix_setup_post_type() {
         'query_var'          => true,
         'rewrite'            => true,
         'capability_type'    => 'post',
-        'has_archive'        => true,
+        'has_archive'        => true, // archive-(post_type).php for show all like a blog
         'hierarchical'       => false,
         'menu_position'      => null,
         'register_meta_box_cb' =>'example_metabox',// name of the function
         //support actions
-        'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' )
+        'taxonomies'         => array('post_tag'),//add taxonomies optional
+        'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt','custom-fields' )
     );
 
-	register_post_type( 'type-name', $args );
+    register_post_type( 'type-name', $args );
+    
+    register_taxonomy('post_tag', 'post_type');// mandatory if want to attach a taxanomy
 }
 add_action( 'init', 'pluginprefix_setup_post_type' );
  
@@ -65,6 +98,13 @@ function pluginprefix_install() {
  
     // clear the permalinks after the post type has been registered
     flush_rewrite_rules();
+
+    //wp version require check
+    global $wp_version;
+    if(version_compare($wp_version,'4.1','<'))
+    {
+        wp_die('This plugin requiers WP version 4.1 or higher');
+    }
 }
 register_activation_hook( __FILE__, 'pluginprefix_install' );
 
@@ -75,14 +115,53 @@ function pluginprefix_deactivation() {
     flush_rewrite_rules();
 }
 register_deactivation_hook( __FILE__, 'pluginprefix_deactivation' );
+// End Create Post Type and flush
+
+// Start Post meta
+
+function example_post_meta_crud()
+{
+    //single value for 1 meta key
+    add_post_meta($post_id,'price','10',true);
+    update_post_meta($post_id, 'price','15'); 
+
+    $price = get_post_meta($post_id, 'price', true);
+    echo $price;
+    delete_post_meta($post_id, 'price');
+
+    //many values for 1 meta key
+    add_post_meta($post_id,'price','10',false);
+    add_post_meta($post_id,'price','15',false);
+    update_post_meta($post_id, 'price','16','10');
+    $prices = get_post_meta($post_id, 'price', true);
+
+    foreach($prices as $price)
+    {
+        echo $price;
+    }
+
+    delete_post_meta($post_id, 'price');// delete all
+    delete_post_meta($post_id, 'price','15');//delete only one
+
+    //retrieve all meta for a post - good because makes only 1 query
+    $all_metadata = get_post_custom($post_id);
+
+    foreach($all_metadata as $key=>$value)
+    {
+        echo $key;
+        foreach($value as $keyVal => $rlval)
+        {
+            echo $keyVal." = > ";
+            echo var_dump($rlval);
+        }
+    }
+}
+
 
 //metabox create
 function example_metabox()
 {
     add_meta_box('example_metabox_customfields','Example Custom Fields','example_metabox_display','type-name','normal');
-    // 4nd param type name
-    //5rd param where to position normal/side
-    //6rd priority
 }
 add_action('add_meta_boxes','example_metabox');
 
@@ -117,9 +196,9 @@ function example_posttype_save($post_id)
     }
 }
 add_action('save_post','example_posttype_save');
+// End Post meta
 
-
-//show all posts with a shortcode
+//show all posts example
 function display_custom_posts()
 {
     $args = array(
@@ -131,7 +210,6 @@ function display_custom_posts()
             'terms' => 'specials' //term
         )
     );
-
     $allPosts = new WP_Query($args);
 	
     $content = " ";	
@@ -140,9 +218,24 @@ function display_custom_posts()
         $content .= the_title();
         $content .= the_content();
     endwhile;
-
     wp_reset_postdata();
     return $content
 }
 add_shortcode('show_all','display_custom_posts')
+
+//get_post_types example
+function exm_get_post_types()
+{
+    $args = array(
+        'public'   => true, 
+        '_builtin' => false //custom types only
+    );
+
+    $post_types = get_post_types($args, 'names', 'and');
+
+    foreach($post_types as $post_type)
+    {
+        echo "<p>".$post_type."</p>";
+    }
+}
 ?>
