6ae7bd86ca6567f5f24b3ba07c2e18ff5d953f12
miroslavgr <43068250+miroslavgr@users.noreply.github.com>
2019-08-20 22:22:41 +0300

    Update general_notes.php

--- general_notes.php
+++ general_notes.php
@@ -6,3 +6,6 @@ jQuery(document).ready(function($) {
     $('#header .share-links, #side-nav-panel .share-links').append('<a href="tel:+844-517-7300" class="top-header-phone">(844) 517-7300</a>');
 //	$('div.archive-products').after('<div class="vc_row wpb_row vc_inner row cbd-graphics" style="margin-bottom: 40px;"><div class="col-md-4"><a href="https://coldfireroasters.com/product/cbd-gourmet-coffee-memberships/" ><img src="https://coldfireroasters.com/wp-content/uploads/2019/06/CBD-COFFEE-Bronze.jpg" class="vc_single_image-img attachment-full"></a></div> <div class="col-md-4"><a href="https://coldfireroasters.com/product/cbd-gourmet-coffee-memberships/" ><img src="https://coldfireroasters.com/wp-content/uploads/2019/06/CBD-COFFEE-Silver.jpg" class="vc_single_image-img attachment-full"></a></div><div class="col-md-4"><a href="https://coldfireroasters.com/product/cbd-gourmet-coffee-memberships/" ><img src="https://coldfireroasters.com/wp-content/uploads/2019/06/CBD-COFFEE-Gold.jpg" class="vc_single_image-img attachment-full"></a></div></div>');
 });
+
+
+document.querySelector("#content > article > div > div > div.cart-v2 > div > div.col-lg-8.col-xl-9 > div > div > form > table > tbody > tr.cart_item > td.product-remove > a").innerHTML = "Remove";
