5ef5b3b59327b46eae031f73d6e7cdafdd6b030b
miroslavgr <43068250+miroslavgr@users.noreply.github.com>
2020-01-03 20:31:28 +0200

    Update explanations.php

--- woocommerce/explanations.php
+++ woocommerce/explanations.php
@@ -1,1 +1,14 @@
 
+Zone = name ( admin side only ) + regions + list shipping methods
+
+Order of zones matters for matching their address, thats why the last uneditable zone is for locations not covered by the others. 
+So the user will lastly go to that. 
+
+Shipping methods
+shipping method = name( for customers ) + bool taxable + cost
+There are 3 core shipping methods for use in the WP Core
+1. Local Pickup 
+2. Flat Rate
+3. Free Shipping - can require min amount and/or coupon for it.
+
+Shipping classes are grouping of proudcts, which can be applied different shipping rates later on.
