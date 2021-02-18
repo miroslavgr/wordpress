bbb976665ffc66d69017a06320f87b47db350e37
miroslavgr <43068250+miroslavgr@users.noreply.github.com>
2021-02-19 01:15:21 +0200

    Update react.js

--- gutenberg/react.js
+++ gutenberg/react.js
@@ -24,3 +24,18 @@ Props are like attributes in html tags like this <Square propVal={1}/>
 If any parent calls Square with any kind of attributes, 
 Square can execute these attributes with 
 this.props.propVal anywhere in the component
+
+Every component which Will only have a render method , can be transformed into function Component
+Function Component is a normal function with name as should be the Component and paramaetrs (props) 
+and this function returns what the render should return , Then this function name becomes executable as a React Object
+Just like the Component itself
+
+function Square(props) {
+	return (<button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>);
+}
+          
+
+          
+  
