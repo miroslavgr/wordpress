f52a42662b582b212c7104d53e5a8091d8629232
miroslavgr <43068250+miroslavgr@users.noreply.github.com>
2021-04-20 15:38:18 +0300

    Create main_notes.php

---
+++ scss/main_notes.php
@@ -0,0 +1,30 @@
+SCSS
+Same as SASS but has curly braces and ; , all css is valid scss
+
+& - parent selector
+
+Top Level Statements
+@use
+@import
+@mixin
+@function
+
+Universal Statements
+@var:value
+@if @each
+
+CSS Statements
+@media
+@font-face
+
+Other Statements
+Property declarations like width: 100px may only be used within style rules and some CSS at-rules
+
+SassScript - Expressions are variable containing css values - passing to functions using globally and so forth
+
+Numbers - 12 or 100px.
+Strings - "Helvetica Neue" or bold.
+Colors - #c6538c or blue.
+
+Variables
+@var:value
